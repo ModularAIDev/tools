c9529a48433274d3464b7da0e583b6cbe46c31dd
Viacheslav Kovalevskyi <viacheslav@kovalevskyi.com>
2024-04-01 15:43:04 -0700

    tools => aic_tools

--- tools/datetime.py
+++
@@ -1,8 +0,0 @@
-from crewai_tools import tool
-
-@tool("Check time")
-def get_time(timezone: str) -> str:
-    """Returns current time in timezone."""
-    # get current time
-    
-    # Function logic here
